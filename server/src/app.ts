a24c41782d4cd3f781f628fcfdd755f3059c8894
yannick2009 <yannickkouakou13@gmail.com>
2024-02-08 18:57:27 +0000

    [backend]-chore: Update Google authentication scope in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -25,7 +25,10 @@ app.use(appSession);
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/login', passport.authenticate('google'));
+app.get(
+	'/login',
+	passport.authenticate('google', { scope: ['profile', 'email'] })
+);
 app.get(
 	'/auth/google/callback',
 	passport.authenticate('google', {
@@ -33,6 +36,10 @@ app.get(
 		failureRedirect: '/failure',
 	})
 );
+app.get('/failure', (req, res) => {
+	res.status(401).json({ message: 'failed to authenticate' });
+});
+
 app.get('/logout', (req, res) => {
 	req.logout(() => {});
 	res.status(200).json({ message: 'logged out' });
