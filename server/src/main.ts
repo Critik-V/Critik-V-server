33fceb46ac49626ac539dfb9c0a42909a197dc40
yannick2009 <yannickkouakou13@gmail.com>
2024-02-10 14:06:46 +0000

    [backend]-chore: Add express-list-routes package and list routes in development mode

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { database, server } from './config';
 import { ErrorHandlers, Panic } from './errors';
 import { commentRouter, notifRouter, postRouter, userRouter } from './routes';
 import { apiRoutePrefix, statusCodes } from './utils';
+import expressListRoutes from 'express-list-routes';
 // -------------------- DATABASE -------------------- //
 database();
 
@@ -20,4 +21,10 @@ app.all('*', (req: Request, res: Response, next: NextFunction) =>
 // -------------------- ERRORS -------------------- //
 app.use(ErrorHandlers);
 // -------------------- SERVER -------------------- //
+if (process.env.NODE_ENV === 'development') {
+	expressListRoutes(userRouter, { prefix: apiRoutePrefix('users') });
+	expressListRoutes(postRouter, { prefix: apiRoutePrefix('posts') });
+	expressListRoutes(commentRouter, { prefix: apiRoutePrefix('comments') });
+	expressListRoutes(notifRouter, { prefix: apiRoutePrefix('notifications') });
+}
 server(app);
