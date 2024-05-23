a18b7bdb8ed18560c6d664ee554870d8dc6a3dd3
yannick2009 <yannickkouakou13@gmail.com>
2024-05-23 10:00:05 +0200

    chores: some modifications in utils tests files

--- server/src/utils/test/logger.test.ts
+++ server/src/utils/test/logger.test.ts
@@ -1,40 +1,40 @@
-import { expect, describe, test } from "vitest";
-import logger from "../logger";
-import chalk from "chalk";
+import { expect, describe, test } from 'vitest';
+import logger from '../logger';
+import chalk from 'chalk';
 
-const table: Function = (
-  name: string,
-  message: string,
-  status: string
-): string => `+-------------------+--------------------+--------------------+
+const table: (name: string, message: string, status: string) => string = (
+	name,
+	message,
+	status
+) => `+-------------------+--------------------+--------------------+
 | ${name}            | ${message}        | ${status}          
 +-------------------+--------------------+--------------------+`;
 
-describe("logger", () => {
-  test("DB connected", () => {
-    expect(typeof logger.successDbLogger).toBe("function");
-    expect(logger.successDbLogger()).toBe(
-      console.log(chalk.green(table("Database", "Database is connected", "✅")))
-    );
-  });
-  test("DB not connected", () => {
-    expect(typeof logger.errorDbLogger).toBe("function");
-    expect(logger.errorDbLogger()).toBe(
-      console.log(
-        chalk.red(table("Database", "Database is not connected", "❌"))
-      )
-    );
-  });
-  test("Server running", () => {
-    expect(typeof logger.successServerLogger).toBe("function");
-    expect(logger.successServerLogger()).toBe(
-      console.log(chalk.green(table("Server", "Server is running", "✅")))
-    );
-  });
-  test("Server not running", () => {
-    expect(typeof logger.errorServerLogger).toBe("function");
-    expect(logger.errorServerLogger()).toBe(
-      console.log(chalk.red(table("Server", "Server is not running", "❌")))
-    );
-  });
+describe('logger', () => {
+	test('DB connected', () => {
+		expect(typeof logger.successDbLogger).toBe('function');
+		expect(logger.successDbLogger()).toBe(
+			console.log(chalk.green(table('Database', 'Database is connected', '✅')))
+		);
+	});
+	test('DB not connected', () => {
+		expect(typeof logger.errorDbLogger).toBe('function');
+		expect(logger.errorDbLogger()).toBe(
+			console.log(
+				chalk.red(table('Database', 'Database is not connected', '❌'))
+			)
+		);
+	});
+	test('Server running', () => {
+		expect(typeof logger.successServerLogger).toBe('function');
+		expect(logger.successServerLogger()).toBe(
+			console.log(chalk.green(table('Server', 'Server is running', '✅')))
+		);
+	});
+	test('Server not running', () => {
+		expect(typeof logger.errorServerLogger).toBe('function');
+		expect(logger.errorServerLogger()).toBe(
+			console.log(chalk.red(table('Server', 'Server is not running', '❌')))
+		);
+	});
 });
