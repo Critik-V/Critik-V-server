8dc825fc64b0d0b2582664f973edbff0fc00841a
yannick2009 <yannickkouakou13@gmail.com>
2024-05-25 01:11:04 +0200

    add github workflow and refactor the .env file

--- server/src/config/server.ts
+++ server/src/config/server.ts
@@ -1,22 +1,22 @@
 // -------------------- IMPORTS -------------------- //
-import { Application } from "express";
-import http from "node:http";
-import logger from "../utils/logger";
+import { Application } from 'express';
+import http from 'node:http';
+import logger from '../utils/logger';
 // -------------------- MAIN -------------------- //
 const server = (app: Application) => {
-  const PORT: number = Number(process.env.PORT) || 5000;
-  const HOST: string = process.env.HOST || "127.0.0.1";
+	const PORT: number = Number(process.env.SERVER_PORT) || 5000;
+	const HOST: string = 'localhost' || '127.0.0.1';
 
-  const httpServer = http.createServer(app);
-  httpServer.listen({ port: PORT, hostname: HOST }, async () => {
-    try {
-      logger.successServerLogger();
-    } catch (error) {
-      logger.errorServerLogger();
-      httpServer.close();
-      process.exit(1);
-    }
-  });
+	const httpServer = http.createServer(app);
+	httpServer.listen({ port: PORT, hostname: HOST }, async () => {
+		try {
+			logger.successServerLogger();
+		} catch (error) {
+			logger.errorServerLogger();
+			httpServer.close();
+			process.exit(1);
+		}
+	});
 };
 // -------------------- EXPORTS -------------------- //
 export default server;
